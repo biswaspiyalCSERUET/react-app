597b278e407314c7fd8ef8c0326b9e320fb42687
biswaspiyalCSERUET <biswaspiyal777@gmail.com>
2020-08-24 08:23:22 +0600

    Updates

---
+++ precache-manifest.2f867fb09304b58bb89da24bdd3c9f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25582093edaa95cce2224ae36435616e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "75faf3161cc88e40fb77",
+    "url": "/react-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2fdd1fbefad3cd3337ce",
+    "url": "/react-app/static/css/main.d5319496.chunk.css"
+  },
+  {
+    "revision": "75faf3161cc88e40fb77",
+    "url": "/react-app/static/js/2.f8f1ea27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app/static/js/2.f8f1ea27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdd1fbefad3cd3337ce",
+    "url": "/react-app/static/js/main.85b6268b.chunk.js"
+  },
+  {
+    "revision": "1c9045e59ad21a5dec7f",
+    "url": "/react-app/static/js/runtime-main.82af57fa.js"
+  }
+]);
